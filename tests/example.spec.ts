7a29538c074cc427897df538f513e684bd7e8163
Jacob Damkjær <jda@testhuset.dk>
2024-05-27 12:32:35 +0200

    Updating test

---
+++ tests/example.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://jda-testhuset.github.io/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/ID-FLÅDEN/);
+});
+
+test('has crew text', async ({ page }) => {
+  await page.goto('https://jda-testhuset.github.io/');
+
+  // Click the BESÆTNING link.
+  await page.getByRole('link', { name: 'BESÆTNING' }).click();
+
+  // Use XPath to locate the element and verify its text content.
+  const element = await page.locator('xpath=/html/body/div[3]/div[4]/h2');
+  await expect(element).toHaveText('Testhusets søfolk');
+});
