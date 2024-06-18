441285a9c94a3de2317936b365756e319fa21a18
Jacob Damkjær <jda@testhuset.dk>
2024-06-18 16:54:29 +0200

    Updated test to use ID

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -14,10 +14,10 @@ test('has crew text', async ({ page }) => {
   await page.getByRole('link', { name: 'BESÆTNING' }).click();
 
   // Use XPath to locate the element
-  const element = await page.locator('xpath=/html/body/div[3]/div[4]/h2');
+  //const element = await page.locator('xpath=/html/body/div[3]/div[4]/h2');
   
   // Use data-testid to locate the element
-  //const element = await page.getByTestId('besaetning');
+  const element = await page.getByTestId('besaetning');
 
   // Verify text on found element
   await expect(element).toHaveText('Testhusets søfolk');
